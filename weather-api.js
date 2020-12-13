3fa4c030371d437987498d783cd98e8c9d508a0c
GSCrawley <gscrawley@gmail.com>
2020-12-13 10:05:04 -0800

    added event listeners to get weather by zip city and geolocation

--- weather-api.js
+++ weather-api.js
@@ -4,8 +4,15 @@ class getWeather {
     this.units = units
   }
 
-  async getWeatherByZip
-}
+  async getWeatherByZip(zip) {
+    const {apiKey, units} = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+    const res = await fetch(path)
+    const json = await res.json()
+
+
+  }
+} 
 
 
 
@@ -13,6 +20,12 @@ function getWeather(zip) {
     const apiKey = '4a3c80e31151dcb42d04a6fac45f5f77'
     const units = 'imperial'
     const path = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=&units=${units}`
+    
+
+    const { base, clouds, cod, eee}
+
+
+
     fetch(path)
       .then(res => res.json())
       .then(json => handleJson(json))
