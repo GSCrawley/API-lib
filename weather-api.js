c9a4d2096fc92a14c2d9d57f3890bce968dc5081
GSCrawley <gscrawley@gmail.com>
2020-12-12 21:24:48 -0800

    moved getWeather function to its own file

--- weather-api.js
+++ weather-api.js
@@ -1,12 +1,10 @@
-function getWeather(zip, apiKey) {
-          
+function getWeather(zip) {
+    const apiKey = '52136878ccc16d9cb8faf89b266167b4'
     const units = 'imperial'
-    const path = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}`
-    fetch(path).then((res) => {
-      return res.json()
-    }).then((json) => {
-      console.log(json)
-    }).catch((err) => {
-      console.log(err.message)
-    })
+    const path = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=&units=${units}`
+    fetch(path)
+      .then(res => res.json())
+      .then(json => handleJson(json))
+      .catch(err => console.log(err.message))
+    
  }
