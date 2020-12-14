b219208b8eb40458331420e75c77763a12631fa2
GSCrawley <gscrawley@gmail.com>
2020-12-13 18:29:27 -0800

    haven't submitted my npm package because it's not working yet. for some reason the event handler is not recognizing an event to handle

--- weather-api.js
+++ weather-api.js
@@ -1,6 +1,6 @@
-class getWeather {
+class MyWeather {
   constructor(apiKey, units = 'imperial') {
-    this.apiKey = apiKey
+    this.apiKey = '4a3c80e31151dcb42d04a6fac45f5f77'
     this.units = units
   }
 
@@ -9,30 +9,41 @@ class getWeather {
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
     const res = await fetch(path)
     const json = await res.json()
-    const { clouds, cod, main, timezone, visibility, weather, wind } = json
+    const { base, clouds, cod, coord, dt, id, main, name, sys, timezone, visibility, weather, wind } = json
+    const { temp, pressure, humidity, temp_max, temp_min } = main
+    const { description, icon } = weather[0]
+    return { temp, pressure, humidity, temp_min, temp_max, clouds, cod, visibility, wind, description, icon }
+
+  }
+  async getWeatherByCity(city) {
+    const {apiKey, units} = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
+    const res = await fetch(path)
+    const json = await res.json()
+    const { base, clouds, cod, coord, dt, id, main, name, sys, timezone, visibility, weather, wind } = json
     const { temp, pressure, humidity, temp_max, temp_min } = main
     const { description, icon } = weather[0]
     return { temp, pressure, humidity, temp_min, temp_max, clouds, cod, visibility, wind, description, icon }
   }
 
-  
-} 
-
-
-
-function getWeather(zip) {
-    const apiKey = '4a3c80e31151dcb42d04a6fac45f5f77'
-    const units = 'imperial'
-    const path = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=&units=${units}`
-    
-
-    const { base, clouds, cod, eee}
-
-
-
-    fetch(path)
-      .then(res => res.json())
-      .then(json => handleJson(json))
-      .catch(err => console.log(err.message))
-    
- }
+  async getWeatherByCityID(cityId) {
+    const {apiKey, units} = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=${units}`
+    const res = await fetch(path)
+    const json = await res.json()
+    const { base, clouds, cod, coord, dt, id, main, name, sys, timezone, visibility, weather, wind } = json
+    const { temp, pressure, humidity, temp_max, temp_min } = main
+    const { description, icon } = weather[0]
+    return { temp, pressure, humidity, temp_min, temp_max, clouds, cod, visibility, wind, description, icon }
+  }
+  async getWeatherByGeo(lat, lon) {
+    const {apiKey, units} = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
+    const res = await fetch(path)
+    const json = await res.json()
+    const { base, clouds, cod, coord, dt, id, main, name, sys, timezone, visibility, weather, wind } = json
+    const { temp, pressure, humidity, temp_max, temp_min } = main
+    const { description, icon } = weather[0]
+    return { temp, pressure, humidity, temp_min, temp_max, clouds, cod, visibility, wind, description, icon }
+  }
+}
